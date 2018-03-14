5aa020e7d689240374dc47fd5de0225d884e8a32
sangsit <boomloveevena@gmail.com>
2018-03-14 11:59:15 +0700

    Design Main UI

---
+++ app/src/main/java/sdu/cs58/sangsit/animalapp/MainActivity.java
@@ -0,0 +1,15 @@
+package sdu.cs58.sangsit.animalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }//end onCreate Method
+
+}//end Class
